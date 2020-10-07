3e0d600cc5d6ddfb02af812babf45f1d3da7acd8
aveWorld <texnarb228@gmail.com>
2020-10-07 12:21:38 +0300

    add helmet to 404 page"

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 export default function NotFound() {
   return (
     <div className="not-found">
+      <Helmet>
+        <title>Not Found</title>
+      </Helmet>
       <h1 className="not-found__title">404 - Page Not Found</h1>
       <Link to="/" className="not-found__link">
         Go Home
